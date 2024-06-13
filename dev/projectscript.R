70516cca59b944d3a525e529e717f298746629ff
priyarai801 <pr801@york.ac.uk>
2024-06-13 11:03:10 +0100

    Initial commit

---
+++ dev/projectscript.R
@@ -0,0 +1,5 @@
+getwd()
+setwd("/Users/priyarai/Documents/Researchproject/data")
+
+#get all three files and save it into separate vectors, but only the column 
+#with the FPKM value for CC-124 Light oxic
