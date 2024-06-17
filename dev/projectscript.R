17720c718f100fb7214f849fdec5886315bbc395
priyarai801 <pr801@york.ac.uk>
2024-06-17 13:12:14 +0100

    Monday 17/6/24
    Working on creating the WGCNA for CC.124.Light.oxic vs CC.124.Dark.anoxic.6.hours

--- dev/projectscript.R
+++ dev/projectscript.R
@@ -79,5 +79,206 @@ p <- ggplot(plot_data_long, aes(x = PC1, y = PC2, color = Sample)) +
 print(p)
 
 #---------------------------------------------------------------------
+#Anyways next step of thermotolerance in wheat was carrying out Differential expression
+#analysis using DESeq2 and the differential expression plots were visualised with volcano
+#plots in the ggplot2 package also used ashr package to shrink the expression fold changes
+#more info in paper) and had log2foldchanges and those with FDR-adjusted p-values were considered
+#further for GO enrichment analysis
 
-#Anyways next step of thermotolerance in wheat 
+#will use merged_df_unique
+
+#DESeq2 only works on raw counts but i have RPKM
+#will use limma package for DE analysis
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("limma")
+library(limma)
+library(dplyr)
+
+#Changes merged_df_unique to be in correct format for DE analysis
+#sets rownames of merged_df_unique to be the gene names in X column
+#Then the select-X will remove the X column and instead just have a column
+#corresponding to the genes
+#This is a useful step for me
+rownames(merged_df_unique) <- merged_df_unique$X
+merged_df_unique <- merged_df_unique %>% select(-X)
+
+#Logtransform the RPKM gene expression values
+log_rpkm <- log2(merged_df_unique + 1)
+
+#Create a design matrix to represent the different conditions and helps with
+#linear modelling i.e. DE analysis
+condition <- factor(c(rep("Light", ncol(merged_df_unique) / 2), rep("Dark", ncol(merged_df_unique) / 2)))
+design <- model.matrix(~ condition)
+
+# Fit the linear model - error here
+fit <- lmFit(log_rpkm, design)
+fit <- eBayes(fit)
+
+#Not sure how to fix error here - but i'll either try DESeq2
+
+#---------------------------------------------------------------------
+#Figure out how to do WGCNA
+
+BiocManager::install("WGCNA")
+library(WGCNA)
+
+#there's an error with my WGCNA -it's not installing completely so i'm missing functions
+#i can't even set soft threshold power - installing it via the tabs seemed
+#to have maybe worked because i don't get the same error
+
+install.packages("RSQLite")
+library(RSQLite)
+
+
+options(repos = c(CRAN = "https://cran.rstudio.com/",
+                  BioCsoft = "https://bioconductor.org/packages/3.17/bioc",
+                  BioCann = "https://bioconductor.org/packages/3.17/data/annotation",
+                  BioCexp = "https://bioconductor.org/packages/3.17/data/experiment",
+                  BioCworkflows = "https://bioconductor.org/packages/3.17/workflows"))
+
+# Check current repositories
+print(getOption("repos"))
+
+# Install WGCNA
+BiocManager::install("WGCNA")
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.19")
+#---------------------------------------------------------------------
+
+# Transpose the data for WGCNA so genes are now columns, and conditions are rows
+#so that it is easier for correlation calculations
+datExpr <- as.data.frame(t(log_rpkm))
+
+
+# Choose a set of soft-thresholding powers
+powers = c(1:10, seq(from = 12, to=20, by=2))
+
+# Call the network topology analysis function
+sft = pickSoftThreshold(datExpr, powerVector = powers, verbose = 5)
+
+# Plot the results:
+par(mfrow = c(1,2))
+cex1 = 0.9
+
+# Scale-free topology fit index as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     xlab = "Soft Threshold (power)", ylab = "Scale Free Topology Model Fit, signed R^2",
+     type = "n", main = paste("Scale independence"))
+text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     labels = powers, cex = cex1, col = "red")
+abline(h = 0.90, col = "red")
+
+# Mean connectivity as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], sft$fitIndices[,5],
+     xlab = "Soft Threshold (power)", ylab = "Mean Connectivity", 
+     type = "n", main = paste("Mean connectivity"))
+text(sft$fitIndices[,1], sft$fitIndices[,5], labels = powers, cex = cex1, col = "red")
+
+softPower = sft$powerEstimate
+
+# Create adjacency matrix
+adjacency = adjacency(datExpr, power = softPower)
+
+# Turn adjacency into Topological Overlap Matrix (TOM)
+TOM = TOMsimilarity(adjacency)
+dissTOM = 1 - TOM
+
+# Perform hierarchical clustering
+geneTree = hclust(as.dist(dissTOM), method = "average")
+
+# Module identification using dynamic tree cut
+dynamicMods = cutreeDynamic(dendro = geneTree, distM = dissTOM,
+                            deepSplit = 2, pamRespectsDendro = FALSE,
+                            minClusterSize = 30)
+
+# Convert labels to colors for plotting
+dynamicColors = labels2colors(dynamicMods)
+
+# Plot the dendrogram and the module colors
+plotDendroAndColors(geneTree, dynamicColors, "Dynamic Tree Cut")
+
+# Define the trait data
+trait_data <- data.frame(
+  Light = c(rep(1, 3), rep(0, 3)),
+  Dark = c(rep(0, 3), rep(1, 3))
+)
+rownames(trait_data) <- colnames(datExpr)
+
+# Module eigengenes
+MEs = moduleEigengenes(datExpr, colors = dynamicColors)$eigengenes
+
+# Correlate modules with traits
+moduleTraitCor = cor(MEs, trait_data, use = "p")
+moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nSamples = nrow(datExpr))
+
+# Plot the correlations
+textMatrix = paste(signif(moduleTraitCor, 2), "\n(",
+                   signif(moduleTraitPvalue, 1), ")", sep = "")
+par(mar = c(6, 8.5, 3, 3))
+labeledHeatmap(Matrix = moduleTraitCor,
+               xLabels = names(trait_data),
+               yLabels = names(MEs),
+               ySymbols = names(MEs),
+               colorLabels = FALSE,
+               colors = blueWhiteRed(50),
+               textMatrix = textMatrix,
+               setStdMargins = FALSE,
+               cex.text = 0.5,
+               zlim = c(-1,1),
+               main = paste("Module-trait relationships"))
+
+#--------------------------------------------------------------------
+#Trying WGCNA again
+
+# Set row names to gene names
+rownames(merged_df_unique) <- merged_df_unique$X
+merged_df_unique <- merged_df_unique %>% select(-X)
+
+# Transpose data for WGCNA
+datExpr <- as.data.frame(t(log_rpkm))
+
+# Remove zero-variance genes
+zero_variance_genes <- which(apply(datExpr, 2, var) == 0)
+if (length(zero_variance_genes) > 0) {
+  datExpr <- datExpr[, -zero_variance_genes]
+}
+
+# Remove rows with any NA values
+datExpr <- na.omit(datExpr)
+
+
+library(WGCNA)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+#This line creates potential candidate values for the soft-threshold
+#power for the WGCNA so the options are 1-10 and 12,14,16,18,20
+powers <- c(1:10, seq(from = 12, to = 20, by = 2))
+#CODE HAS BEEN RUN UP TO THIS POINT
+
+#With the candidate values in powers, below code will choose which
+#value should be chosen as the soft threshold power
+#the pickSoftThreshold function does this
+#datExpr is the input data
+#powerVector are the values in powers
+#verbose is the level of info u want to get
+#5 is the highest it tells you the connectivity + free topology fit index
+sft <- pickSoftThreshold(datExpr, powerVector = powers, verbose = 5)
+
+#ERROR HERE??????????????? - this is what it says
+# > sft <- pickSoftThreshold(datExpr, powerVector = powers, verbose = 5)
+# pickSoftThreshold: will use block size 141.
+# pickSoftThreshold: calculating connectivity for given powers...
+# ..working on genes 1 through 141 of 141
+# Error in summary(lm1)$coefficients[2, 1] : subscript out of bounds
+
+#going to write out datExpr as a file and attach it if it can find
+#anything wrong
+
+# Write datExpr to a CSV file
+write.csv(datExpr, file = "datExpr.csv", row.names = TRUE)
